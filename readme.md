42399a53d832ea4b731ab6f52424b0caef1bf3e9
Waleed Jubeh <wsameer70@gmail.com>
2019-09-14 15:27:25 +0300

    Create UnitTest for HospitalA

--- readme.md
+++ readme.md
@@ -8,7 +8,11 @@ Unfortunately,HospitalA system stores its data in XML file which is different fr
 So they can't connect the both system to interactive with each other,there software engineers were unable to connect them,for that  they call you (Waleed Jubeh)
 to connect the two systems and solve the issue.
 
-You decide to make an adapter to connect both systems.
+## Prototype 
+I decide to make an adapter to connect both systems.
+The adapter is work in both direction so it's convert from XML to JSON and from JSON to XML
+
+The Adapter converts HospitalB to be like HospitalA system which means it's reacted like HospitalA system.
 
 ## Tech environment and tools :
 
@@ -18,3 +22,7 @@ You decide to make an adapter to connect both systems.
 ## Class diagram for the adapter
 
 ![Class diagram](http://waleedjubeh.esy.es/files/adapter.png)
+
+## Result
+
+The prototype is doable and can be applied to connect between the systems
---
+++ test/com/company/HospitalATest.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import com.sun.source.tree.AssertTree;
+import org.junit.Assert;
+import  org.junit.Assert.*;
+import  org.junit.Test;
+
+public class HospitalATest {
+    HospitalA tester=new HospitalA();
+    @Test
+    public void addPatient() {
+        Patient p=new Patient("Waleed","Jubeh");
+        tester.addPatient(p);
+        Assert.assertEquals( tester.getPatients().get(0),p);
+    }
+
+    @Test
+    public void exportXML() {
+        tester.addPatient(new Patient("Waleed","Jubeh"));
+        tester.addPatient(new Patient("Ahmad","Karaki"));
+        tester.addPatient(new Patient("Tameem","Tamimi"));
+        try{
+            tester.ExportXML("testData");
+            Assert.assertTrue(true);
+
+        }catch (Exception e){
+            Assert.assertTrue("Exception Error",false);
+
+        }
+
+
+    }
+
+    @Test
+    public void importXML() {
+        tester.ImportXML("testData.xml");
+    }
+
+    @Test
+    public void getPatients() {
+        tester.addPatient(new Patient("Waleed","Jubeh"));
+        tester.addPatient(new Patient("Ahmad","Karaki"));
+        tester.addPatient(new Patient("Tameem","Tamimi"));
+        Assert.assertTrue(tester.getPatients().size()==3);
+    }
+}
